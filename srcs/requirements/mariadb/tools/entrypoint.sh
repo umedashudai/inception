9e890664574609b5893a02bc2b3bfa3bd162a7c5
umedashudai <umeda20030613@icloud.com>
2026-05-02 16:21:08 +0900

    change

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -6,9 +6,9 @@ echo "CREATE SCHEMA IF NOT EXITSTS $db_name ;" > db.sql
 echo "CREATE USER IF NOT EXITSTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db.sql
 echo "GRANT ALL ON menagerie.* TO '$db_user'@'%' ;" >> db.sql
 echo "ALTER USER 'root'@'localhost' INDENTIFIED BY '12345' ;" >> db.sql
-echo "FLUSH PRIVILEGES ;"
+echo "FLUSH PRIVILEGES ;" >> db.sql
 
-mysql < db1.sql
+mysql < db.sql
 
 mysqladmin -u root -p"${db_root_pwd}" shutdown
 
