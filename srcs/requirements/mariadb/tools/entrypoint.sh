6dc1b436a40d7e834aad42745962f680306bb682
umedashudai <umeda20030613@icloud.com>
2026-05-04 11:22:52 +0000

    remake

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-service mysql start
+set -e
 
-echo "CREATE SCHEMA IF NOT EXITSTS $db_name ;" > db.sql
-echo "CREATE USER IF NOT EXITSTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db.sql
-echo "GRANT ALL ON menagerie.* TO '$db_user'@'%' ;" >> db.sql
-echo "ALTER USER 'root'@'localhost' INDENTIFIED BY '12345' ;" >> db.sql
-echo "FLUSH PRIVILEGES ;" >> db.sql
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
-mysql < db.sql
+service mariadb start
+
+if [ ! -d "/var/lib/mysql/${db_name}" ]; then
+mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS \`${db_name}\`;
+CREATE USER IF NOT EXISTS '${db_user}'@'%' IDENTIFIED BY '${db_pwd}';
+GRANT ALL PRIVILEGES ON \`${db_name}\`.* TO '${db_user}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${db_root_pwd}';
+FLUSH PRIVILEGES;
+EOF
+fi
 
 mysqladmin -u root -p"${db_root_pwd}" shutdown
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # create directory to use in nginx container later and also to setup the wordpress conf
 mkdir -p /var/www/
 mkdir -p /var/www/html
@@ -25,7 +23,7 @@ mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 mv /wp-config.php /var/www/html/wp-config.php
 
 
-sed -i -r "s/db1/$db_name/1"   wp-config.php
+sed -i -r "s/db/$db_name/1"   wp-config.php
 sed -i -r "s/user/$db_user/1"  wp-config.php
 sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
 
@@ -40,18 +38,18 @@ wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 wp theme install astra --activate --allow-root
 
 
-wp plugin install redis-cache --activate --allow-root
+# wp plugin install redis-cache --activate --allow-root
 
 wp plugin update --all --allow-root
 
 
  
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir /run/php
 
 
 
-wp redis enable --allow-root
+# wp redis enable --allow-root
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.4 -F
